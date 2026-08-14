16e5cba4eb34a272f34c29a46a0a4c47f78c50d9
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-14 11:34:45 +0530

    PHASE 1: TypeScript Fundamentals solving

---
+++ day2-ts_working_with_react/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// - Type Inference and Annotation
+// let a: string = "amit";
+// a = "sanjeet";
+// a = "pol";
+// console.log(a);
+Object.defineProperty(exports, "__esModule", { value: true });
+// let status = "damage" // not available
+let status = "success";
+//# sourceMappingURL=index.js.map
--- day2-ts_working_with_react/index.ts
+++ day2-ts_working_with_react/index.ts
@@ -1,2 +1,64 @@
-let a = 80;
-console.log(a);
+// - Type Inference and Annotation
+// let a: string = "amit";
+// a = "sanjeet";
+// a = "pol";
+// console.log(a);
+
+// - Primitive Types
+// N -number
+// let a: number = 56;
+// N - null
+// let empty: null = null;
+// B - bigint
+// let bigint: bigint = 5566455n;
+// B - boolean
+// let istrue: boolean = true;
+// S - string
+// let b: string = "amit";
+// S - symbol
+// let s: symbol = Symbol("hello");
+// U - undefined
+// let u: undefined = undefined;
+
+// - Arrays
+// any
+// let a: any = 879;
+// a = "sita";
+// a = true;
+// unknown
+// let a: unknown = 879;
+// a = "sita";
+// a = true;
+
+// never
+
+// let a:never = "amit";
+// console.log(a.toUpperCase());
+
+// let a: never;
+// let arr: any[] = [1, 2, 3, true, false, "amit", 12566454n, null, undefined];
+
+// - Tuples
+// let arr: [number, string, boolean] = [12, "amit", true];
+// let data: [
+//   { name: string },
+//   { age: number },
+//   { istrue: boolean },
+//   { isEmpty: null },
+// ] = [{ name: "amit" }, { age: 26 }, { istrue: false }, { isEmpty: null }];
+// - enum - options
+// enum Role {
+//   ADMIN,
+//   SUP_ADMIN,
+//   USER,
+// }
+// let role: Role = Role.USER;
+// - Any vs Unknown vs Never
+// - Union Types
+// let yolo: number | string = "amit";
+// yolo = 26;
+
+// - Literal Types
+type Status = "pending" | "success" | "error";
+// let status = "damage" // not available
+let status: Status = "success";
